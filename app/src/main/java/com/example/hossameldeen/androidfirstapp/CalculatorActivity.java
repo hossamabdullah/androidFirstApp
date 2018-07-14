893ab5a2ff997b1846f53efd28975fb0a5f541c0
Hossameldeen <hossamabdalh@gmail.com>
2018-07-14 15:53:27 +0200

    fragments didnot work, let move their code inside main activity :D

--- app/src/main/java/com/example/hossameldeen/androidfirstapp/CalculatorActivity.java
+++ app/src/main/java/com/example/hossameldeen/androidfirstapp/CalculatorActivity.java
@@ -1,11 +1,25 @@
 package com.example.hossameldeen.androidfirstapp;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 public class CalculatorActivity extends AppCompatActivity {
+    private FragmentManager manager;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calculator);
+
+        manager = getSupportFragmentManager();
+
+        DisplayFragment displayFragment = DisplayFragment.newInstance();
+        InputFragment inputFragment = InputFragment.newInstance();
+
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.cont_fragment_display, displayFragment);
+        transaction.add(R.id.cont_fragment_input, inputFragment);
+        transaction.commit();
     }
 }

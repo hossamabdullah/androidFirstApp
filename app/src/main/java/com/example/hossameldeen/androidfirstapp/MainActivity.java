0b2a48e4056a0bc60f4d3ec34c2911c80d8d1481
Hossameldeen <hossamabdalh@gmail.com>
2018-07-14 16:41:45 +0200

    remove titlebar

--- app/src/main/java/com/example/hossameldeen/androidfirstapp/MainActivity.java
+++ app/src/main/java/com/example/hossameldeen/androidfirstapp/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
         final TextView message = (TextView) findViewById(R.id.lbl_display);
